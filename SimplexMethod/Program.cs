ab808674ddf2e4a028a58753ea9f76f754a5cea7
anastasea <anastasea1001@mail.ru>
2018-04-22 22:51:43 +0300

    Partially working, errors with -b

--- SimplexMethod/Program.cs
+++ SimplexMethod/Program.cs
@@ -7,41 +7,10 @@ namespace SimplexMethod
         static void Main(string[] args)
         {
             LinearProgrammingProblem pr = new LinearProgrammingProblem(true, new double[] { 5,3,-6 }, new double[,]
-            {{-2,3,0}, {1,-2,-1}, {0,3,-2}  }, new double[] { 5,4,2 }, new MathSign[] { MathSign.LessThan, MathSign.GreaterThan, MathSign.Equal }, null);
+            {{-2,3,0}, {1,-2,-1}, {0,3,-2}  }, new double[] { -5,4,2 }, new MathSign[] { MathSign.LessThan, MathSign.GreaterThan, MathSign.Equal }, null);
 
             Simplex sm = new Simplex(pr);
-            double d = 0; double[] dd = new double[3];
-            sm.Calculate(ref d, ref dd);
-            for(int i = 0; i < sm.SimplexTable.GetLength(0); i++)
-            {
-                for(int j =0; j < sm.SimplexTable.GetLength(1); j++)
-                {
-                    Console.Write(sm.SimplexTable[i, j] + "   ");
-                }
-                Console.WriteLine();
-            }
-            foreach(int i in sm.SlackVarInd)
-            {
-                Console.Write(i + " ");
-            }
-            Console.WriteLine();
-            foreach (int i in sm.SurplusVarInd)
-            {
-                Console.Write(i + " ");
-            }
-            Console.WriteLine();
-            foreach (int i in sm.ArtificialVarInd)
-            {
-                Console.Write(i + " ");
-            }
-            Console.WriteLine("basis");
-            foreach (int i in sm.Basis)
-            {
-                Console.Write(i + " ");
-            }
-            Console.WriteLine();
-
-            Console.WriteLine("Hello World!");
+            sm.Calculate();
             Console.ReadKey();
         }
     }
--- SimplexMethod/Simplex.cs
+++ SimplexMethod/Simplex.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace SimplexMethod
 {
@@ -14,21 +15,24 @@ namespace SimplexMethod
         // n+1 строка - оценки
         // n+2 строка - оценки: коэффициент при M
         // 0 столбец - свободные члены
-        // m+1 столбец - 
         // Базис хранится отдельно
         public int[] Basis { get; set; }
         public List<int> SlackVarInd { get; set; }
         public List<int> SurplusVarInd { get; set; }
         public List<int> ArtificialVarInd { get; set; }
         private int varInd;
+        private int costMRow;
+        private int costRow;
         private const double M = double.PositiveInfinity;
 
         public Simplex(LinearProgrammingProblem prob)
         {
             Prob = prob;
             SimplexTable = new double[Prob.CountConstraint + 3,
-                Prob.CountVariables + CountTotalNewVars() + 2];
+                Prob.CountVariables + CountTotalNewVars() + 1];
             varInd = Prob.CountVariables + 1;
+            costMRow = SimplexTable.GetLength(0) - 1;
+            costRow = costMRow - 1;
             Basis = new int[Prob.CountConstraint];
             for (int i = 0; i < Prob.CountConstraint + 1; i++)
             {
@@ -88,8 +92,7 @@ namespace SimplexMethod
             MakeAllVarNonNegative();
             AddSlackAndSurplusVariables();
         }
-
-        // Приведение задачи минимизации к задаче максимизации min(F)=max(-F)
+        
         public void ToMax()
         {
             if (Prob.Maximize == false)
@@ -100,24 +103,21 @@ namespace SimplexMethod
                 }
             }
         }
-
-        // Обеспечение положительности столбца свободных членов 
+        
         public void MakeConstantsPositive()
         {
             for (int i = 0; i < Prob.CountConstraint; i++)
             {
                 if (Prob.Constants[i] <= 0)
                 {
-                    for(int j = 0; j < SimplexTable.GetLength(1) - 1; j++)
+                    for(int j = 0; j < SimplexTable.GetLength(1); j++)
                     {
                         SimplexTable[i + 1, j] *= -1;
                     }
                 }
             }
         }
-
-        // При присутствии переменных без ограничения на неотрицательность вводятся новые переменные x1 и x2
-        // с данным ограничением такие, что x = x1 - x2
+        
         public void MakeAllVarNonNegative()
         {
             if (Prob.NotNonNegativeVarInd != null)
@@ -177,7 +177,6 @@ namespace SimplexMethod
                 {
                     ArtificialVarInd.Add(varInd);
                     SimplexTable[i + 1, varInd] = 1;
-                    // SimplexTable[0, varInd] = Double.PositiveInfinity;
                     SimplexTable[0, varInd] = -M;
                     Basis[i] = varInd;
                     varInd++;
@@ -189,7 +188,7 @@ namespace SimplexMethod
         {
             int costMRow = SimplexTable.GetLength(0) - 1;
             int costRow = costMRow - 1;
-            for (int j = 0; j < SimplexTable.GetLength(1) - 1; j++)
+            for (int j = 0; j < SimplexTable.GetLength(1); j++)
             {
                 SimplexTable[costRow, j] = 0;
                 SimplexTable[costMRow, j] = 0;
@@ -215,22 +214,169 @@ namespace SimplexMethod
             }
         }
 
-        public void Calculate(ref double optObjectiveValue, ref double[] optSolution)
+        public Tuple<double, double[]> Calculate()
         {
             ConvertToStandardForm();
             AddArtificialVariables();
+            Console.WriteLine();
+            Console.WriteLine("INIT BASIS");
+            foreach (int i in Basis)
+            {
+                Console.Write(i + " ");
+            }
             FindCosts();
+            Console.WriteLine();
+            Console.WriteLine("INIT TABLE");
+            for (int i = 0; i < SimplexTable.GetLength(0); i++)
+            {
+                for (int j = 0; j < SimplexTable.GetLength(1); j++)
+                {
+                    Console.Write(SimplexTable[i, j] + "   ");
+                }
+                Console.WriteLine();
+            }
+            while (true)
+            {
+                FindCosts();
+                int pivotColumn = FindPivotColumn();
+                Console.WriteLine("PIV COL: " + pivotColumn);
+                if (pivotColumn == -1)
+                {
+                    // Проверка на равентсво нулю исскуственной переменной: если нет, то нет оптимального решения
+                    if (SimplexTable[SimplexTable.GetLength(0) - 1, 0] != 0)
+                    {
+                        return null;
+                    }
+                    else
+                    {
+                        double[] optSolution = new double[SimplexTable.GetLength(1)-1];
+                        foreach(int i in Basis)
+                        {
+                            optSolution[i - 1] = SimplexTable[i, 0];
+                        }
+                        double optObjectiveValue = SimplexTable[SimplexTable.GetLength(0) - 2, 0];
+                        Console.WriteLine("OPT SOL:");
+                        foreach(double v in optSolution)
+                        {
+                            Console.Write(v + " ");
+                        }
+                        Console.WriteLine();
+                        Console.WriteLine("OPT VAL:");
+                        Console.WriteLine(optObjectiveValue);
+                        return new Tuple<double, double[]>(optObjectiveValue, optSolution);
+                    }
+                }
+                else
+                {
+                    int pivotRow = FindPivotRow(pivotColumn);
+                    Console.WriteLine("PIV ROW: " + pivotRow);
+                    if (pivotRow == -1)
+                    {
+                        return null;
+                    }
+                    Basis[pivotRow - 1] = pivotColumn;
+                    Console.WriteLine("NEW BASE: ");
+                    foreach (int i in Basis)
+                    {
+                        Console.Write(i + " ");
+                    }
+                    Console.WriteLine();
+                    for (int i = 1; i < SimplexTable.GetLength(0); i++)
+                    {
+                        if (i == pivotRow)
+                            continue;
+                        for (int j = 0; j < SimplexTable.GetLength(1); j++)
+                        {
+                            SimplexTable[i, j] = SimplexTable[i, j] - (SimplexTable[i, pivotColumn] * SimplexTable[pivotRow, j])/ SimplexTable[pivotRow, pivotColumn];
+                        }
+                    }
+                    for (int j = 0; j < SimplexTable.GetLength(1); j++)
+                    {
+                        SimplexTable[pivotRow, j] = SimplexTable[pivotRow, j] / SimplexTable[pivotRow, pivotColumn];
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("NEW TABLE");
+                    for (int i = 0; i < SimplexTable.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < SimplexTable.GetLength(1); j++)
+                        {
+                            Console.Write(SimplexTable[i, j] + "   ");
+                        }
+                        Console.WriteLine();
+                    }
+                    
+                }
+            }
         }
 
-        
+        // rewrite later
         private int FindPivotColumn()
         {
-            return 0;
+            List<int> minInd = new List<int>();
+            double minM = SimplexTable[costMRow, 1];
+            for (int j = 2; j < SimplexTable.GetLength(1); j++)
+            {
+                if(SimplexTable[costMRow, j] < minM)
+                {
+                    minM = SimplexTable[costMRow, j];
+                }
+            }
+            for (int j = 1; j < SimplexTable.GetLength(1); j++)
+            {
+                if (SimplexTable[costMRow, j] == minM)
+                {
+                    minInd.Add(j);
+                }
+            }
+            if(minInd.Count == 1)
+            {
+                return minInd[0];
+            }
+            else
+            {
+                double min = SimplexTable[costRow, 1];
+                int pivotCol = minInd[0];
+                foreach(int j in minInd)
+                {
+                    if (SimplexTable[costRow, j] < min)
+                    {
+                        min = SimplexTable[costRow, j];
+                        pivotCol = j;
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine("min " + min + " minM " + minM);
+                if(min >= 0 && minM >=0)
+                {
+                    return -1;
+                }
+                else 
+                    return pivotCol;
+            }
         }
 
-        private int FindPivotRow()
+        private int FindPivotRow(int pivotCol)
         {
-            return 0;
+            int pivotRow = -1;
+            for (int i = 1; i < SimplexTable.GetLength(0) - 2; i++)
+            {
+                if(SimplexTable[i, pivotCol] > 0)
+                {
+                    pivotRow = i;
+                    break;
+                }
+            }
+            if (pivotRow == -1)
+                return pivotRow;
+            for(int i = pivotRow + 1; i < SimplexTable.GetLength(0) - 2; i++)
+            {
+                if((SimplexTable[i, pivotCol] > 0) && ((SimplexTable[i, 0] / SimplexTable[i, pivotCol]) < 
+                    (SimplexTable[pivotRow, 0] / SimplexTable[pivotRow, pivotCol])))
+                {
+                    pivotRow = i;
+                }
+            }
+            return pivotRow;
         }
     }
 }
